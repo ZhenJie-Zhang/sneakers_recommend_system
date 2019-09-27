b6dd6e42450b03845652e13f765a50e650838f9d
harryhung465 <53025855+harryhung465@users.noreply.github.com>
2019-09-27 18:12:58 +0800

    Add files via upload

--- scraper/ruten/change_encoding.py
+++ scraper/ruten/change_encoding.py
@@ -2,8 +2,10 @@
 import pandas as pd
 # first read_csv in encoding = "目前的code"
 # file_from = "./ruten.csv"  #<--where is file from
+#code = "950"
 file_from = eval(input("where is file from (ex:'./ruten.csv'): "))
-df = pd.read_csv(file_from, encoding = "950") # find encoding to decoding file
+code = eval(input("encoding (ex:'950')= "))
+df = pd.read_csv(file_from, encoding = code) # find encoding to decoding file
 print(df)
 print(df.columns) # check columns
 print("-"*20)
@@ -13,6 +15,7 @@ new_encoding_file = eval(input("where does new file save to (ex:'./ruten_1.csv')
 df1 = df.to_csv(new_encoding_file, encoding = "utf-8-sig", index = False)
 print("-"*20)
 # third check to decoding file
+print("check")
 df2 = pd.read_csv(new_encoding_file, encoding = "utf-8-sig")
 print(df2)
 print(df.columns) # check columns
